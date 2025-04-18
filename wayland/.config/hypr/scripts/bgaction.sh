a699d3af85358956fd97c811095691b3ec7ef20d
Rav4n-T <leonsgmali@gmail.com>
2025-04-18 15:03:04 +0800

    add git hypr config

---
+++ wayland/.config/hypr/scripts/bgaction.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+#start swwww
+swww query || swww init
+
+#figure out which waybar theme is set
+THEMEIS=$(readlink -f ~/.config/waybar/style.css | cut -d '-' -f2)
+
+#show the correct wallpapper based on the theme
+if [ $THEMEIS == "dark.css" ]; then
+  swww img --resize crop -o eDP-1  $HOME/.config/hypr/background/bg.png
+  # swww img  -o HDMI-A-1  $HOME/.config/hypr/background/009.png
+else
+  swww img --resize crop -o eDP-1  $HOME/.config/hypr/background/bg.png
+  # swww img  -o HDMI-A-1  $HOME/.config/hypr/background/009.png
+fi
