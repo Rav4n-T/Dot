dd7525c11857b25d95a348a6c3c12fcbea7919ea
Rav4n-T <leonsgmali@gmail.com>
2025-04-23 15:53:10 +0800

    disabled smear-cursor.nvim

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -9,17 +9,17 @@ return {
 	-- 		},
 	-- 	},
 	-- },
-	{
-		"sphamba/smear-cursor.nvim",
-		opts = {
-			cursor_color = "#ff8800",
-			stiffness = 0.3,
-			trailing_stiffness = 0.1,
-			trailing_exponent = 5,
-			hide_target_hack = true,
-			gamma = 1,
-		},
-	},
+	-- {
+	-- 	"sphamba/smear-cursor.nvim",
+	-- 	opts = {
+	-- 		cursor_color = "#ff8800",
+	-- 		stiffness = 0.3,
+	-- 		trailing_stiffness = 0.1,
+	-- 		trailing_exponent = 5,
+	-- 		hide_target_hack = true,
+	-- 		gamma = 1,
+	-- 	},
+	-- },
 	{
 		"lewis6991/gitsigns.nvim",
 		enabled = false,
