b4372bfc0b092f223f994c28a2d71f3ab83f1ae3
Rav4n-T <leonsgmali@gmail.com>
2025-05-03 15:00:20 +0800

    change lazy.nvim colorschema to gruvbox-material

--- nvim/.config/nvim/lua/config/load_plugins.lua
+++ nvim/.config/nvim/lua/config/load_plugins.lua
@@ -33,7 +33,7 @@ function M.setup()
 
 		-- Configure any other settings here. See the documentation for more details.
 		-- colorscheme that will be used when installing plugins.
-		install = { colorscheme = { "everforest" } },
+		install = { colorscheme = { "gruvbox-material" } },
 		-- automatically check for plugin updates
 		checker = { enabled = false },
 
